a843c83ce83f22403062def18f7ffa68528813ea
JimmyGeji0321 <22451102@life.hkbu.edu.hk>
2023-04-09 20:32:24 +0800

    add 8

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,5 @@
 # chatbot.py
 import pymysql
-import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -74,7 +73,7 @@ ex: /photo victoria, /photo changzhou
 2. Type '/video' to see cooking videos 
 ex: /video cooking
 3. Type '/review' to write/read TV show reviews
-ex: /review write, /review read''')
+ex: /review write movieName review, /review read''')
 
 
 def photo(update: Update, context: CallbackContext) -> None:
@@ -96,11 +95,14 @@ def video(update: Update, context: CallbackContext) -> None:
 
 
 def review(update: Update, context: CallbackContext) -> None:
+    print(context)
+    print(context.args)
     msg = context.args[0]
     if msg == 'write':
-        movie_name = context.args[1]
-        movie_review = context.args[2:]
-        add_review(movie_name, movie_review)
+        print("A")
+        # movie_name = context.args[1]
+        # movie_review = context.args[]
+        # add_review(movie_name, movie_review)
     elif msg == 'read':
         results = show_reviews()
         for i in results:
