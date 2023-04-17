3a1bf32b2f0ed28da8ecd94f5546cbcb33b22499
JimmyGeji0321 <126168430+JimmyGeji0321@users.noreply.github.com>
2023-04-17 22:54:43 +0800

    add 46

--- chatbot.py
+++ chatbot.py
@@ -9,11 +9,10 @@ import configparser
 import logging
 
 
+
+
 def db_connect():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    db = pymysql.connect(host=config['AMAZONS']['HOST'], user=config['AMAZONS']['USER'],
-                         password=config['AMAZONS']['PASSWORD'], database=config['AMAZONS']['DATABASE'])
+    db = pymysql.connect(host='cloudcomputing.c8zsz8bm7sbi.us-east-1.rds.amazonaws.com', user='admin', password='swQ50wDD9xXEuGZwDpva', database='chatbot')
     return db
 
 
@@ -38,9 +37,8 @@ def add_review(movie_name, movie_review):
         cursor.execute(sql)
         db.commit()
         db.close()
-        return 1
     except:
-        return 0
+        print("Error")
 
 
 def main():
@@ -55,117 +53,85 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
 
-    # # register a dispatcher to handle message:
+    # # register a dispatcher to handle message: here we register an echo dispatcher
     menu_handler = MessageHandler(Filters.text & (~Filters.command), show_menu)
     dispatcher.add_handler(menu_handler)
 
     # # on different commands - answer in Telegram
-    # dispatcher.add_handler(CommandHandler("photo", photo))
-    # dispatcher.add_handler(CommandHandler("video", video))
+    # dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("photo", photo))
+    dispatcher.add_handler(CommandHandler("video", video))
     dispatcher.add_handler(CommandHandler("review", review))
+    dispatcher.add_handler(CommandHandler('maths', game))
     dispatcher.add_handler(CallbackQueryHandler(answer))
-
-    #dispatcher.add_handler(CommandHandler('game', game))
-    #dispatcher.add_handler(CallbackQueryHandler(answer))
-
     # To start the bot:
     updater.start_polling()
     updater.idle()
 
 
 def show_menu(update, context):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='view a photo of HK', callback_data='photo')],
-        [InlineKeyboardButton(text='watch a cooking video', callback_data='video')],
-        [InlineKeyboardButton(text='read some movie reviews', callback_data='read')],
-        [InlineKeyboardButton(text='write a movie review', callback_data='write')],
-        #[InlineKeyboardButton(text='play a maths game', callback_data='game')],
-    ])
-    update.message.reply_text('''Hello! Welcome to CC chatbot!
-What can I help you?
-Please select the following buttons:
-''', reply_markup=keyboard)
-    #update.message.reply_text('''BTW if you want to play a maths game, please type /game''')
+    update.message.reply_text('''Hello! Welcome to CC chatbot! 
+Please select the following functions:
 
-def answer(update: Update, context: CallbackContext) -> None:
-    msg = update.callback_query.data
-    if msg == 'photo':
-        src = 'img/' + str(random.randint(1, 11)) + '.jpg'
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(src, 'rb'))
-    elif msg == 'video':
-        src = 'video/' + str(random.randint(1, 6)) + '.mp4'
-        context.bot.send_video(chat_id=update.effective_chat.id, video=open(src, 'rb'))
-    elif msg == 'read':
-        results = show_reviews()
-        reviews = ''''''
-        for i in results:
-            reviews = reviews + i[1] + ''':
-''' + i[2] + '''
-            
-'''
-        context.bot.send_message(chat_id=update.effective_chat.id, text=reviews)
-    elif msg == 'write':
-        context.bot.send_message(chat_id=update.effective_chat.id, text='''please use the following format:
-/review [movieName] [review]''')
-    #elif msg == 'game':
-    #    game()
+1. Type '/photo' to see hiking photos 
+ex: /photo victoria, /photo changzhou
+2. Type '/video' to see cooking videos 
+ex: /video cooking
+3. Type '/review' to read TV show reviews
+ex: /review read''')
+
+
+def photo(update: Update, context: CallbackContext) -> None:
+    msg = context.args[0]
+    if msg == 'victoria':
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/t1-2.jpg', 'rb'))
+    elif msg == 'changzhou':
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/t2-2.jpg', 'rb'))
+    else:
+        update.message.reply_text("Sorry, I didn't have any photos related to " + msg + "!")
+
+
+def video(update: Update, context: CallbackContext) -> None:
+    msg = context.args[0]
+    if msg == 'cooking':
+        context.bot.send_video(chat_id=update.effective_chat.id, video=open('video/2.mp4', 'rb'))
+    else:
+        update.message.reply_text("Sorry, I didn't have any videos related to " + msg + "!")
+
 
 def review(update: Update, context: CallbackContext) -> None:
-    movie_name = context.args[0]
-    movie_review = ' '.join(context.args[1:])
-    if add_review(movie_name, movie_review) == 1:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='''Your reviews have been saved successfully!!''')
+    msg = context.args[0]
+    # if msg == 'write':
+    #     movie_name = context.args[1]
+    #     movie_review = context.args[2]
+    #     # add_review(movie_name, movie_review)
+    #     print(movie_name, movie_review)
+    if msg == 'read':
+        results = show_reviews()
+        for i in results:
+            update.message.reply_text(i[1] + ': ' + i[2])
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='''Failed! Please use the correct format''')
+        update.message.reply_text("Please input the right command!")
 
 
-# def photo(update: Update, context: CallbackContext) -> None:
-#     msg = context.args[0]
-#     if msg == 'victoria':
-#         context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/t1-2.jpg', 'rb'))
-#     elif msg == 'changzhou':
-#         context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/t2-2.jpg', 'rb'))
-#     else:
-#         update.message.reply_text("Sorry, I didn't have any photos related to " + msg + "!")
-#
-#
-# def video(update: Update, context: CallbackContext) -> None:
-#     msg = context.args[0]
-#     if msg == 'cooking':
-#         context.bot.send_video(chat_id=update.effective_chat.id, video=open('video/2.mp4', 'rb'))
-#     else:
-#         update.message.reply_text("Sorry, I didn't have any videos related to " + msg + "!")
-#
-#
-# def review(update: Update, context: CallbackContext) -> None:
-#     msg = context.args[0]
-#     if msg == 'write':
-#         movie_name = context.args[1]
-#         movie_review = ' '.join(context.args[1:])
-#         add_review(movie_name, movie_review)
-#     elif msg == 'read':
-#         results = show_reviews()
-#         for i in results:
-#             update.message.reply_text(i[1] + ': ' + i[2])
-#     else:
-#         update.message.reply_text("Please input the right command!")
+if __name__ == '__main__':
+    main()
 
 
 
+def game(update, context):
+    a, b = randint(1, 100), randint(1, 100)
+    update.message.reply_text('{} + {} = ?'.format(a, b),
+        reply_markup = InlineKeyboardMarkup([[
+                InlineKeyboardButton(str(s), callback_data = '{} {} {}'.format(a, b, s)) for s in range(a + b - randint(1, 3), a + b + randint(1, 3))
+            ]]))
 
-#def game(update, context):
-#    a, b = randint(1, 100), randint(1, 100)
-#    update.message.reply_text('{} + {} = ?'.format(a, b),
-#        reply_markup = InlineKeyboardMarkup([[
-#                InlineKeyboardButton(str(s), callback_data = '{} {} {}'.format(a, b, s)) for s in range(a + b - randint(1, 3), a + b + randint(1, 3))
-#            ]]))
-
-#def answer(update, context):
-#    a, b, s = [int(x) for x in update.callback_query.data.split()]
-#    if a + b == s:
-#        update.callback_query.edit_message_text('Correct! You must smarter than Grade 1 students :)')
-#    else:
-#        update.callback_query.edit_message_text('Wrong! OMG, you are even worse than Grade 1 students :(')
+def answer(update, context):
+    a, b, s = [int(x) for x in update.callback_query.data.split()]
+    if a + b == s:
+        update.callback_query.edit_message_text('Correct! You must smarter than Grade 1 students :)')
+    else:
+        update.callback_query.edit_message_text('Wrong! OMG, you are even worse than Grade 1 students :(')
 
 
 
